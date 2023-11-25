d1e095795b2fe12fa224de5ab361ca3cdbf90cb7
Abdulaziz <151828455+Abdulaziz-Middle@users.noreply.github.com>
2023-11-25 16:44:31 +0500

    Add files via upload

---
+++ AdminPanel1/src/App.js
@@ -0,0 +1,12 @@
+import Menu from "./Components/Menu";
+import Container from "./Components/Container";
+import "./App.css";
+function App() {
+  return (
+    <div className="App">
+      <Menu />
+      <Container />
+    </div>
+  );
+}
+export default App;
---
+++ AdminPanel1/src/Components/TopSeller.js
@@ -0,0 +1,13 @@
+import seller1 from "../img/seller1.jpg";
+import seller2 from "../img/seller2.jpg";
+import seller3 from "../img/seller3.jpg";
+import seller4 from "../img/seller4.jpg";
+import seller5 from "../img/seller5.jpg";
+const TopSeller = [
+  { id: 1, seller_name: "Abdulazizbek", username: "Abdulazizbek802@gmail.com", imgSrc: seller1 },
+  { id: 2, seller_name: "Abdulaziz", username: "Abdulaziz@gmail.com", imgSrc: seller2 },
+  { id: 3, seller_name: "Azizbek", username: "Azizbek@gmail.com", imgSrc: seller3 },
+  { id: 4, seller_name: "Msi", username: "MSI@gmail.com", imgSrc: seller4 },
+  { id: 5, seller_name: "Abdulaziz802", username: "Abdulaziz802@gmail.com", imgSrc: seller5 },
+];
+export default TopSeller;
